2dd9ab29adb01cfddfbf3a569d0b8c480dba8eaa
Maqaro <basilnasr@outlook.com>
2024-08-16 07:07:25 +0100

    UI work (header and control section)

--- linked-list/src/App.jsx
+++ linked-list/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
+import Header from './modules/Header'
+import List from './modules/List'
 import './App.css'
 
 function App() {
-
-  return (
-    <>
-
-    </>
-  )
+	return (
+		<>
+			<Header/>
+			<List/>
+		</>
+	)
 }
 
 export default App
---
+++ linked-list/src/modules/Header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <div className="Header">
+            <h1>Linked List Visualiser</h1>
+        </div>
+    )
+}
+
+export default Header
