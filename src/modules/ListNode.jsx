b06404216de34487fe879c53a9c2093bb965d1c5
Maqaro <basilnasr@outlook.com>
2024-08-17 18:20:02 +0100

    added arrows between the nodes

--- src/modules/ListNode.jsx
+++ src/modules/ListNode.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 function ListNode({ value, index, next }) {
-
     return (
-        <div className='node'>
-            <span className='nodeValue'>{value}</span>
-            <p className='nodeIndex'>{`Index: ${index}`}</p>
-            <p className='nodeNext'>{`Next: ${next == null ? 'null' : next}`}</p>
+        <div className='nodeWrapper'>
+            <div className='node'>
+                <span className='nodeValue'>{value}</span>
+                <p className='nodeIndex'>{`Index: ${index}`}</p>
+                <p className='nodeNext'>{`Next: ${next == null ? 'null' : next}`}</p>
+            </div>
+            {next !== null && <div className='arrow'>→</div>}
         </div>
     );
 }
